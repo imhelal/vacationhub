c84fe63836095ff54d372e5b9cd12e91547b7a88
Helal <imhelal.dev@gmail.com>
2024-10-03 23:17:32 +0600

    Owl Carousel & Responsive

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+/* initilize counterup */
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
+
+/* Owl Carousel One */
+$(".toure-places").owlCarousel({
+  stagePadding: 70,
+  loop: true,
+  margin: 30,
+  dots: false,
+  nav: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      margin: 10,
+      stagePadding: 30,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+/* Owl Carousel Two */
+$(".testimonials").owlCarousel({
+  loop: true,
+  margin: 30,
+  dots: false,
+  nav: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
